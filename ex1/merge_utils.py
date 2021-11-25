b70f0dae26b3fee7553ce472ab1119729b65e240
itay1542 <itay1542@gmail.com>
2021-11-25 11:18:39 +0200

    split merge functions to in place and copy

--- ex1/merge_utils.py
+++ ex1/merge_utils.py
@@ -1,19 +1,32 @@
-def merge_dicts(source, dest):
+import copy
+
+
+def merge_dicts(source: dict, dest: dict):
+    source_copy, dest_copy = copy.deepcopy(source), copy.deepcopy(dest)
+    return __merge_dicts_in_place(source_copy, dest_copy)
+
+
+def __merge_dicts_in_place(source: dict, dest: dict):
     for key, value in source.items():
         if isinstance(value, dict):
             deep_val = dest.setdefault(key, {})
-            merge_dicts(value, deep_val)
+            __merge_dicts_in_place(value, deep_val)
         elif isinstance(value, list):
             deep_val = dest.setdefault(key, [])
-            merge_lists(value, deep_val)
+            __merge_lists_in_place(value, deep_val)
         else:
             if dest.setdefault(key, value) != value:
                 dest[key] = [dest[key], value]
     return dest
 
 
-def merge_lists(source_list, dest):
-    for item in source_list:
+def merge_lists(source: list, dest: list):
+    source_copy, dest_copy = copy.deepcopy(source), copy.deepcopy(dest)
+    return __merge_lists_in_place(source_copy, dest_copy)
+
+
+def __merge_lists_in_place(source: list, dest: list):
+    for item in source:
         if __is_last_hierarchy(item) or not isinstance(item, dict) or len(dest) == 0:
             dest.append(item)
         elif isinstance(item, dict):
@@ -25,11 +38,10 @@ def merge_lists(source_list, dest):
                     to_append.append(item)
             dest.append(to_append) if len(to_append) > 0 else None
 
-
-def __is_last_hierarchy(target_dict):
+def __is_last_hierarchy(target_dict: dict):
     for value in target_dict.values():
         return not isinstance(value, list) and not isinstance(value, dict)
 
 
-def __are_keys_mergeable(source_dict, dest_dict):
+def __are_keys_mergeable(source_dict: dict, dest_dict: dict):
     return set(source_dict.keys()).issubset(dest_dict.keys())
