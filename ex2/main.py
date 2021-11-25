bbf7011d1d92ee0b55946a8aad956cf89459d711
itay1542 <itay1542@gmail.com>
2021-11-25 11:20:06 +0200

    visual improvements

--- ex2/main.py
+++ ex2/main.py
@@ -1,5 +1,6 @@
 from ex2.requirements_processing import RequirementsProcessor
 
 if __name__ == '__main__':
-    print(RequirementsProcessor('.\\inputs').requirements_file_to_string('requirements.txt'))
+    req_processor = RequirementsProcessor('.\\inputs')
+    print(req_processor.requirements_file_to_string('requirements.txt'))
 
